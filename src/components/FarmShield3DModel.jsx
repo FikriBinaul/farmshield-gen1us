76414481928cc8f2d15f775dd4a95a9e653c31ce
FikriBinaul <fikribinaulumah@gmail.com>
2025-11-24 20:29:53 +0700

    Update FarmShield3DModel.jsx

--- src/components/FarmShield3DModel.jsx
+++ src/components/FarmShield3DModel.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
-import { useLoader } from "@react-three/fiber";
 
 function FarmShieldModel() {
   const geometry = useLoader(STLLoader, "/models/farmshield.stl");
@@ -25,16 +25,18 @@ export default function FarmShield3DModel() {
   return (
     <div className="w-full h-full">
       <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-        <FarmShieldModel />
-        <OrbitControls 
-          enableZoom={true}
-          autoRotate 
-          autoRotateSpeed={0.15}
-          minDistance={2}
-          maxDistance={10}
-          enablePan={true}
-          makeDefault
-        />
+        <Suspense fallback={null}>
+          <FarmShieldModel />
+          <OrbitControls
+            enableZoom={true}
+            autoRotate
+            autoRotateSpeed={0.15}
+            minDistance={2}
+            maxDistance={10}
+            enablePan={true}
+            makeDefault
+          />
+        </Suspense>
       </Canvas>
     </div>
   );
