159720e5bf32fd9a7beae4bb83e223e0a682ca40
FikriBinaul <fikribinaulumah@gmail.com>
2025-11-27 00:28:20 +0700

    Adjust camera position in FarmShield3DModel

--- src/components/FarmShield3DModel.jsx
+++ src/components/FarmShield3DModel.jsx
@@ -29,7 +29,7 @@ function FarmShieldModel() {
 export default function FarmShield3DModel() {
   return (
     <div className="w-full h-full">
-      <Canvas camera={{ position: [0, 3, 8], fov: 75 }}>
+      <Canvas camera={{ position: [0, 0, 8], fov: 75 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
         <directionalLight position={[-10, -10, -5]} intensity={0.5} />
